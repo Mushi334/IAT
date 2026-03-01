7cb8711a2f8e1df441d0fdc050fe764207ee7b25
Mushi334 <hossainmd.sahal@gmail.com>
2026-03-01 15:48:59 +0600

    Update raceiat.js

--- raceiat.js
+++ raceiat.js
@@ -7,7 +7,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
             name : 'Male', //Will appear in the data.
             title : {
                 media : {word : 'Male'}, //Name of the category presented in the task.
-                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                css : {color:'#31940F','font-size':'2em', 'font-family':'"Noto Sans Bengali", sans-serif'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
@@ -16,7 +16,11 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
                 {word: 'Samir'},
                 {word: 'Turjo'},
                 {word: 'Tushar'},                 
-                {word: 'Asif'}     
+                {word: 'Asif'},
+                {word: 'Ibrahim'},
+                {word: 'Abdullah'},
+                {word: 'রায়হান'},
+                {word: 'অভিষেক'}
             ],
             //Stimulus css (style)
             stimulusCss : {color:'#31940F','font-size':'2.3em'}
@@ -25,7 +29,7 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
             name : 'Female', //Will appear in the data.
             title : {
                 media : {word : 'Female'}, //Name of the category presented in the task.
-                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                css : {color:'#31940F','font-size':'2em', 'font-family':'"Noto Sans Bengali", sans-serif'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
@@ -34,7 +38,11 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
                 {word: 'Nafisa'},
                 {word: 'Shoily'},
                 {word: 'Sumaiya'},
-                {word: 'Anjila'}
+                {word: 'Anjila'},
+                {word: 'Ahona'},
+                {word: 'Meem'},
+                {word: 'চন্দ্রিকা'},
+                {word: 'ইসরাত'}
             ],
             //Stimulus css (style)
             stimulusCss : {color:'#31940F','font-size':'2.3em'}
@@ -43,18 +51,20 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
             name : 'Science',
             title : {
                 media : {word : 'Science'},
-                css : {color:'#0000FF','font-size':'1.8em'},
+                css : {color:'#0000FF','font-size':'2em', 'font-family':'"Noto Sans Bengali", sans-serif'},
                 height : 4 //Used to position the "Or" in the combined block.
             },
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: 'Chemistry'},
-                {word: 'Physics'},
-                {word: 'Mathematics'},
-                {word: 'Pharmacy'},
-                {word: 'Biology'},
-                {word: 'Geology'},
-                {word: 'Engineering'},
-                {word: 'Medical'}
+                {word: 'chemistry'},
+                {word: 'physics'},
+                {word: 'mathematics'},
+                {word: 'pharmacy'},
+                {word: 'biology'},
+                {word: 'geology'},
+                {word: 'engineering'},
+                {word: 'medical'},
+                {word: 'রসায়ন'},
+                {word: 'গণিত'}
             ],
             //Stimulus css
             stimulusCss : {color:'#0000FF','font-size':'2.3em'}
@@ -63,18 +73,20 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
             name : 'Humanities',
             title : {
                 media : {word : 'Humanities'},
-                css : {color:'#0000FF','font-size':'1.8em'},
+                css : {color:'#0000FF','font-size':'2em', 'font-family':'"Noto Sans Bengali", sans-serif'},
                 height : 4 //Used to position the "Or" in the combined block.
             },
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {word: 'History'},
-                {word: 'Philosophy'},
-                {word: 'Anthropology'},
-                {word: 'Art History'},
-                {word: 'Linguistics'},
-                {word: 'Literature'},
-                {word: 'Sociology'},
-                {word: 'Theology'}
+                {word: 'history'},
+                {word: 'philosophy'},
+                {word: 'anthropology'},
+                {word: 'fine arts'},
+                {word: 'english'},
+                {word: 'literature'},
+                {word: 'sociology'},
+                {word: 'religious studies'},
+                {word: 'ইতিহাস'},
+                {word: 'দর্শনশাস্ত্র'}
             ],
             //Stimulus css
             stimulusCss : {color:'#0000FF','font-size':'2.3em'}
